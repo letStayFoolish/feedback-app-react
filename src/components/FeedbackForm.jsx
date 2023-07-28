88cd49376b7fac2b992265f54031b1a26cc40449
stayFoolish <nemanjakaraklajic90@icloud.com>
2023-07-28 11:31:57 +0200

    Custom Button Component

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,5 +1,6 @@
 import Card from './shared/Card'
 import {useState} from "react";
+import Button from "./shared/Button";
 const FeedbackForm = () => {
   const [text, setText] = useState('')
   function handleTextChange(e) {
@@ -12,7 +13,7 @@ const FeedbackForm = () => {
         {/*@todo - rating select component*/}
         <div className="input-group">
           <input onChange={handleTextChange} type="text" value={text} placeholder='Write a review' />
-          <button type='submit'>Send</button>
+          <Button type='submit' >Send</Button>
         </div>
       </form>
     </Card>
