6378b32f6f4b786b1201369c2906231705efce2b
stayFoolish <nemanjakaraklajic90@icloud.com>
2023-07-28 21:11:15 +0200

    Add Feedback

--- src/App.js
+++ src/App.js
@@ -4,8 +4,14 @@ import FeedbackData from "./data/FeedbackData";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
+import {v4 as uuidv4 } from 'uuid'
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
+  function addFeedback(newFeedback) {
+    newFeedback.id = uuidv4()
+    setFeedback([newFeedback, ...feedback])
+  }
+
   function deleteFeedback(id) {
     if (window.confirm(`Aree you sure?`)) {
       setFeedback([...feedback].filter((item) => item.id !== id))
@@ -15,7 +21,7 @@ function App() {
     <>
       <Header text='Feedback' />
       <div className='container'>
-        <FeedbackForm />
+        <FeedbackForm handleAdd={addFeedback} />
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} deleteFeedback={deleteFeedback} />
       </div>
--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -2,7 +2,7 @@ import Card from './shared/Card'
 import {useState} from "react";
 import Button from "./shared/Button";
 import RatingSelect from "./RatingSelect";
-const FeedbackForm = () => {
+const FeedbackForm = ({ handleAdd }) => {
   const [btnDisabled, setBtnDisabled] = useState(true)
   const [text, setText] = useState('')
   const [message, setMessage] = useState('')
@@ -20,9 +20,21 @@ const FeedbackForm = () => {
     }
     setText(e.target.value)
   }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    if (text.trim().length > 10) {
+      const newFeedback = {
+        rating,
+        text
+      }
+      handleAdd(newFeedback)
+      setText('')
+    }
+  }
   return (
     <Card>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>How would you rate your service with us></h2>
         {/*@todo - rating select component*/}
         <RatingSelect select={(rating) => setRating(rating)} />
